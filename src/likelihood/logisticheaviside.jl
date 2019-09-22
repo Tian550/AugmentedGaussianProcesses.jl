469afb02cefad972c3aace2012130d1c0c4fc96c
Tian550 <38570771+Tian550@users.noreply.github.com>
2019-09-22 16:05:26 +0200

    Update logisticheaviside.jl

--- src/likelihood/logisticheaviside.jl
+++ src/likelihood/logisticheaviside.jl
@@ -75,35 +75,45 @@ function local_updates!(model::VGP{T,LogisticHeavisideLikelihood{T},AnalyticVI{T
             k = model.likelihood.y_class[i] ## k stands for the label of ith observation
             if k == j
                 model.likelihood.c[k][i] = 0 ## There is no kth polya-gamma variable
-                model.likelihood.θ[k][i] = 0 ## There is no kth polya-gamma variable
             else
                 t = model.nFeatures ## t is the number of row elements
                 model.likelihood.c[j][i] = sqrt((model.μ[k][i])^2 + (model.μ[j][i])^2 + model.Σ[k][(i-1)*t+i] + model.Σ[j][(i-1)*t+i])
-                ## updates for model.likelihood.θ
-                model.likelihood.θ[j][i] = 0.5/model.likelihood.c[j][i]*tanh(0.5*model.likelihood.c[j][i])
             end
+        end
+    end
+
+    ## updates for model.likelihood.θ
+    for i in 1:model.nFeatures
+        for j in 1:model.nLatent
+            model.likelihood.θ[j][i] = 0.5*(1/model.likelihood.c[j][i])*(exp(model.likelihood.c[j][i])-1)/(1+exp(model.likelihood.c[j][i]))
         end
     end
 end
 
 function local_updates!(model::SVGP{T,LogisticHeavisideLikelihood{T},AnalyticVI{T},V}) where {T<:Real,V<:AbstractVector{T}}
     ## updates for model.likelihood.c
-    for i in 1:model.inference.nSamplesUsed
+    seq = 1
+    for i in model.inference.MBIndices
         for j in 1:model.nLatent
-            k = model.likelihood.y_class[model.inference.MBIndices[i]]
+            k = model.likelihood.y_class[i]
             if k == j
-                model.likelihood.c[k][i] = 0
-                model.likelihood.θ[k][i] = 0
+                model.likelihood.c[k][seq] = 0
             else
-                model.likelihood.c[j][i] =  sqrt(abs2(dot(view(model.κ[k],i,:),model.μ[k])) +
-                                            abs2(dot(view(model.κ[j],i,:),model.μ[j])) +
-                                            transpose(view(model.κ[k],i,:)) * model.Σ[k] * view(model.κ[k],i,:) +
-                                            transpose(view(model.κ[j],i,:)) * model.Σ[j] * view(model.κ[j],i,:) +
-                                            model.K̃[j][i] +
-                                            model.K̃[k][i])
-                ## updates for model.likelihood.θ
-                model.likelihood.θ[j][i] = 0.5/model.likelihood.c[j][i]*tanh(0.5*model.likelihood.c[j][i])
+                model.likelihood.c[j][seq] =  sqrt(abs2(dot(view(model.κ[k],seq,:),model.μ[k])) +
+                                            abs2(dot(view(model.κ[j],seq,:),model.μ[j])) +
+                                            transpose(view(model.κ[k],seq,:)) * model.Σ[k] * view(model.κ[k],seq,:) +
+                                            transpose(view(model.κ[j],seq,:)) * model.Σ[j] * view(model.κ[j],seq,:) +
+                                            model.K̃[j][seq] +
+                                            model.K̃[k][seq])
             end
+        end
+        seq += 1
+    end
+
+    ## updates for model.likelihood.θ
+    for i in 1:length(model.inference.MBIndices)
+        for j in 1:model.nLatent
+            model.likelihood.θ[j][i] = 0.5*(1/model.likelihood.c[j][i])*(exp(model.likelihood.c[j][i])-1)/(1+exp(model.likelihood.c[j][i]))
         end
     end
 end
@@ -122,6 +132,7 @@ function sample_local!(model::VGP{T,<:LogisticHeavisideLikelihood,<:GibbsSamplin
     end
     return nothing
 end
+
 
 ## Global Gradient Section ##
 
@@ -144,17 +155,19 @@ end
 
 function ∇E_μ(model::SVGP{T,<:LogisticHeavisideLikelihood}) where {T}
     e = Vector([zeros(model.inference.nSamplesUsed) for _ in 1:model.nLatent])
-    for i in model.inference.nSamplesUsed
+    seq = 1
+    for i in model.inference.MBIndices
         for j in 1:model.nLatent
-            k = model.likelihood.y_class[model.inference.MBIndices[i]]
+            k = model.likelihood.y_class[i]
             if k == j
                 container = [l for l in 1:model.nLatent]
                 deleteat!(container, k)
-                e[k][i] = (model.nLatent - 1)/2 + sum(model.likelihood.θ[a][i] * dot(view(model.κ[a],i,:),model.μ[a]) for a in container)
+                e[k][seq] = (model.nLatent - 1)/2 + sum(model.likelihood.θ[a][seq] * dot(view(model.κ[a],seq,:),model.μ[a]) for a in container)
             else
-                e[j][i] = -0.5 + model.likelihood.θ[j][i] * dot(view(model.κ[k],i,:),model.μ[k])
+                e[j][seq] = -0.5 + model.likelihood.θ[j][seq] * dot(view(model.κ[k],seq,:),model.μ[k])
             end
         end
+        seq += 1
     end
     return e
 end
@@ -163,17 +176,19 @@ end
 
 function ∇E_Σ(model::AbstractGP{T,<:LogisticHeavisideLikelihood}) where {T}
     v = deepcopy(model.likelihood.θ)
-    for i in model.inference.nSamplesUsed ## Traversing all the observations
+    seq = 1
+    for i in model.inference.MBIndices ## Traversing all the observations
         for j in 1:model.nLatent ## Traversing all the labels
-            k = model.likelihood.y_class[model.inference.MBIndices[i]] ## k stands for the label of ith observation
+            k = model.likelihood.y_class[i] ## k stands for the label of ith observation
             if k == j
                 container = [l for l in 1:model.nLatent]
                 deleteat!(container, k)
-                v[k][i] = sum([model.likelihood.θ[a][i] for a in container])
+                v[k][seq] = sum([model.likelihood.θ[a][seq] for a in container])
             else
-                v[j][i] = model.likelihood.θ[j][i]
+                v[j][seq] = model.likelihood.θ[j][seq]
             end
         end
+        seq += 1
     end
     return v
 end
@@ -194,9 +209,10 @@ function expecLogLikelihood(model::VGP{T,<:LogisticHeavisideLikelihood,<:Analyti
             if k == j
                 continue
             else
+                t = model.nFeatures
                 tot +=  0.5*(model.μ[k][i]-model.μ[j][i]-
-                        (abs2(model.μ[k][i])+abs2(model.μ[j][i])+model.Σ[k][i,i] +
-                        model.Σ[j][i,i])*model.likelihood.θ[j][i])
+                        (abs2(model.μ[k][i])+abs2(model.μ[j][i])+model.Σ[k][(i-1)*t+i] +
+                        model.Σ[j][(i-1)*t+i])*model.likelihood.θ[j][i])
             end
         end
     end
@@ -206,21 +222,23 @@ end
 
 function expecLogLikelihood(model::SVGP{T,<:LogisticHeavisideLikelihood,<:AnalyticVI}) where {T}
     tot = 0
-    for i in 1:model.inference.nSamplesUsed
+    seq = 1
+    for i in model.inference.MBIndices
         for j in 1:model.nLatent
-            k = model.likelihood.y_class[model.inference.MBIndices[i]]
+            k = model.likelihood.y_class[i]
             if k == j
                 continue
             else
-                tot += 0.5* (dot(view(model.κ[k],i,:),model.μ[k]) -
-                            dot(view(model.κ[j],i,:),model.μ[j]) -
-                            (abs2(dot(view(model.κ[k],i,:),model.μ[k])) +
-                            abs2(dot(view(model.κ[j],i,:),model.μ[j])) +
-                            transpose(view(model.κ[k],i,:)) * model.Σ[k] * view(model.κ[k],i,:) +
-                            transpose(view(model.κ[j],i,:)) * model.Σ[j] * view(model.κ[j],i,:)) *
-                            model.likelihood.θ[j][i])
+                tot += 0.5* (dot(view(model.κ[k],seq,:),model.μ[k]) -
+                            dot(view(model.κ[j],seq,:),model.μ[j]) -
+                            (abs2(dot(view(model.κ[k],seq,:),model.μ[k])) +
+                            abs2(dot(view(model.κ[j],seq,:),model.μ[j])) +
+                            transpose(view(model.κ[k],seq,:)) * model.Σ[k] * view(model.κ[k],seq,:) +
+                            transpose(view(model.κ[j],seq,:)) * model.Σ[j] * view(model.κ[j],seq,:)) *
+                            model.likelihood.θ[j][seq])
             end
         end
+        seq += 1
     end
     tot -= model.inference.nSamplesUsed*logtwo
     return model.inference.ρ*tot
@@ -229,8 +247,8 @@ end
 function PolyaGammaKL(model::VGP{T,<:LogisticHeavisideLikelihood,<:AnalyticVI}) where {T}
     tot = 0
     for i in 1:model.nSample
-        k = model.likelihood.y_class[i]
         for j in 1:model.nLatent
+            k = model.likelihood.y_class[i]
             if k == j
                 continue
             else
@@ -243,19 +261,20 @@ end
 
 function PolyaGammaKL(model::SVGP{T,<:LogisticHeavisideLikelihood,<:AnalyticVI}) where {T}
     tot = 0
-    for i in 1:model.inference.nSamplesUsed
-        k = model.likelihood.y_class[model.inference.MBIndices[i]]
+    seq = 1
+    for i in model.inference.MBIndices
         for j in 1:model.nLatent
+            k = model.likelihood.y_class[i]
             if k == j
                 continue
             else
-                tot += -log(cosh(0.5*model.likelihood.c[j][i])) + 0.5*abs2(model.likelihood.c[j][i])*model.likelihood.θ[j][i]
+                tot += -log(cosh(0.5*model.likelihood.c[j][seq])) + 0.5*abs2(model.likelihood.c[j][seq])*model.likelihood.θ[j][seq]
             end
         end
+        seq += 1
     end
     return -(model.inference.ρ * tot)
 end
-
 
 function sample_global!(model::VGP{T,<:LogisticHeavisideLikelihood,<:GibbsSampling}) where {T}
     model.Σ .= inv.(Symmetric.(Diagonal.(2.0.*∇E_Σ(model)).+model.invKnn))
